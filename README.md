93e1911b1229495fb211321438762f405671b6d5
dmitry.khominich <dmitry.khominich@datadvance.net>
2017-04-29 23:27:40 +0300

    add image, figure, files support. add Readme.md

--- README.md
+++ README.md
@@ -1,2 +1,127 @@
-# loggingbot
+# Loggingbot
 Add Telegram Bot to python's logging outputs
+
+Below are code example and Telegram's Chat view.
+
+## Code example
+
+```python
+import os 
+import sys
+sys.path.append(os.path.expanduser('~/code/loggingbot'))
+from loggingbot import TelegramBotHandler
+```
+
+Telegram bot token (take it from [@botfather](https://telegram.me/botfather) ) and list of user ids where to send the messages are defined below. 
+
+Note that this users should first start conversation the the bot (by telegram policy bots cannot send messages to unknown users). You can get particular user id by sending a message to bot and the check the updates from HTTP API like this `https://api.telegram.org/botXXXXXXXXX:XXXXXXXXXXXXX_XXXXXXXXXXXXXXXXXXXXX/getUpdates`.
+
+
+```python
+token = 'XXXXXXXXX:XXXXXXXXXXXXX_XXXXXXXXXXXXXXXXXXXXX'
+user_list = ['YYYYYYYYY']
+```
+
+Let's set logging level to `INFO` for root logger. Then create a new log event handler `TelegramBotHandler` and attach it to root logger.
+
+Note that [pyTelegramBotAPI](https://github.com/eternnoir/pyTelegramBotAPI) should be installed to
+work with Telegram bot.
+
+
+```python
+import logging
+logging.root.setLevel(logging.INFO)
+tbh = TelegramBotHandler(token, user_list)
+logging.root.addHandler(tbh)
+```
+
+As usual let's define some custom logger named `myapp`
+
+
+```python
+logger = logging.getLogger('myapp')
+```
+
+Everything is setup! Let's just use it! In order to send the message to bot you should provide `extra` dictionary argument into logging functions. You should define `bot` key to `True`.
+
+
+```python
+logger.info('My Application is launched', extra={'bot': True})
+```
+
+    INFO:myapp:My Application is launched
+
+
+
+```python
+try:
+    x = 1 / 0
+except Exception as e:
+    logger.error("Huston, we've got a problem: %s", str(e), extra={'bot': True})
+```
+
+    ERROR:myapp:Huston, we've got a problem: integer division or modulo by zero
+
+
+By the way, you can skip message to be sent to bot by avoiding `bot` key in `extra` dictionary.
+
+
+```python
+logger.info('This message will not be sent to bot')
+```
+
+    INFO:myapp:This message will not be sent to bot
+
+
+The nice thing that you can send to chat not only text but text along with multimedia: **images, files and even matplotlib's figures**!
+
+
+```python
+# the demo taken from http://matplotlib.org/examples/shapes_and_collections/scatter_demo.html
+"""
+Simple demo of a scatter plot.
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+N = 50
+x = np.random.rand(N)
+y = np.random.rand(N)
+colors = np.random.rand(N)
+area = np.pi * (15 * np.random.rand(N))**2  # 0 to 15 point radii
+
+plt.scatter(x, y, s=area, c=colors, alpha=0.5)
+
+logger.info('Sending an image..', extra={'bot': True, 'figure': plt.gcf()})
+```
+
+    INFO:myapp:Sending an image..
+
+
+
+![png](files/output_13_1.png)
+
+
+The images or files can be sent by providing its path or file object:
+
+
+```python
+with open(os.path.expanduser('~/Downloads/kittens.jpg')) as fin:
+    logger.info('Everyone loves kittens!', extra={'bot': True, 'image': fin})
+```
+
+    INFO:myapp:Everyone loves kittens!
+
+
+
+```python
+logger.fatal('Catch the dump!', extra={'bot': True, 'file': '~/coredump.txt'})
+```
+
+    CRITICAL:myapp:Catch the dump!
+
+
+## How it looks like in Telegram
+
+![png](files/example.png)
--- loggingbot.py
+++ loggingbot.py
@@ -5,41 +5,7 @@ import logging
 import uuid
 import json
 import StringIO
-
-
-class ToFolderHandler(logging.Handler):
-    """
-    Write log record to json-file with unique filename in directory `dir`
-
-    Usage:
-    logging.root.addHandler(ToFileHandler('~/.logmessages'))
-    """
-    def __init__(self, dir):
-        """
-        Initialize the handler.
-        """
-        self.dir = os.path.abspath(os.path.expanduser(dir))
-        if not os.path.exists(self.dir):
-            os.makedirs(self.dir)
-        if not os.path.isdir(self.dir):
-            raise RuntimeError('{} is not a directory'.format(self.dir))
-        logging.Handler.__init__(self)
-
-    def emit(self, record):
-        """
-        Emit a record.
-
-        """
-        try:
-            self.acquire()
-            with open(os.path.join(self.dir, str(uuid.uuid4())), 'w') as fout:
-                json.dump(record.__dict__, fout, indent=1)
-                # pickle.Pickler(fout).save_dict(record.__dict__)
-            self.release()
-        except (KeyboardInterrupt, SystemExit):
-            raise
-        except:
-            self.handleError(record)
+from io import BytesIO
 
 
 class TelegramBotHandler(logging.StreamHandler):
@@ -53,14 +19,14 @@ class TelegramBotHandler(logging.StreamHandler):
     logging.warning('Hello world!', extra={'bot': True})
 
     """
-    def __init__(self, botid, bot_users=[], encoding=None):
+    def __init__(self, botid, bot_users=[]):
 
         logging.StreamHandler.__init__(self)
         self.stream = None
         self.botid = botid
         self.bot_users = bot_users
-        self.encoding = encoding
         self.formatter = logging.Formatter(fmt=logging.BASIC_FORMAT)
+        self.figure_width = 800
         # try to init bot
         try:
             import telebot
@@ -80,12 +46,66 @@ class TelegramBotHandler(logging.StreamHandler):
         """
         Emit a record.
         """
-        if ('bot' in record.__dict__) and record.__dict__['bot']:
-            self.stream = StringIO.StringIO()
-            logging.StreamHandler.emit(self, record)
-            try:
-                if self._bot:
+
+        if not self._bot:
+            return
+
+        if ('bot' in record.__dict__) and (record.__dict__['bot']):
+            if len(record.msg) > 0:
+                self.stream = StringIO.StringIO()
+                logging.StreamHandler.emit(self, record)
+                try:
                     for id in self.bot_users:
                         self._bot.send_message(id, self.stream.getvalue())
-            except:
-                self.handleError(record)
+                except:
+                    self.handleError(record)
+            if 'figure' in record.__dict__:
+                fig = record.__dict__['figure']
+                import matplotlib
+                from matplotlib import rcParams
+                if isinstance(fig, matplotlib.figure.Figure):
+                    kw = dict( format='png'
+                             , facecolor=fig.get_facecolor()
+                             , edgecolor=fig.get_edgecolor()
+                             # , dpi=rcParams['savefig.dpi']
+                             , dpi=self._adjust_dpi(fig)
+                             , bbox_inches='tight'
+                             )
+                    bytes_io = BytesIO()
+                    fig.canvas.print_figure(bytes_io, **kw)
+                    try:
+                        for id in self.bot_users:
+                            bytes_io.seek(0)
+                            self._bot.send_photo(id, bytes_io)
+                    except:
+                        self.handleError(record)
+            if 'image' in record.__dict__:
+                image = record.__dict__['image']
+                if isinstance(image, basestring):
+                    try:
+                        image = open(os.path.expanduser(image), 'rb')
+                    except:
+                        return
+                try:
+                    for id in self.bot_users:
+                        image.seek(0)
+                        self._bot.send_photo(id, image)
+                except:
+                    self.handleError(record)
+            if 'file' in record.__dict__:
+                _file = record.__dict__['file']
+                if isinstance(_file, basestring):
+                    try:
+                        _file = open(os.path.expanduser(_file), 'rb')
+                    except:
+                        return
+                try:
+                    for id in self.bot_users:
+                        _file.seek(0)
+                        self._bot.send_document(id, _file)
+                except:
+                    self.handleError(record)
+
+    def _adjust_dpi(self, f):
+        w, h = f.get_size_inches()
+        return self.figure_width / w * 1.065
