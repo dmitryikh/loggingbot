283369aad1d4d33085ff154ad6434ebe7cdd3685
Guillelerial <guillermo.gc1994@gmail.com>
2020-09-20 17:37:51 +0200

    fixed version name error

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ from setuptools import find_packages, setup
 
 setup(name='loggingbot',
       description='Helper class to provide a logging handler linked to a Telegram Bot',
-      version='master',
       python_requires='>=3.7',
       install_requires=[
             'pyTelegramBotAPI==3.7.2',
